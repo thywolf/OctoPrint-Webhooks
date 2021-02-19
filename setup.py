89ef01c2cfbc7d425ba7b72423b71785c43b07f7
thywolf <thywolf@gmail.com>
2021-02-19 21:34:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ plugin_package = "octoprint_webhooks"
 plugin_name = "OctoPrint-Webhooks"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "3.0.3"
+plugin_version = "3.0.3t"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
 plugin_description = """When certain events occur, a custom webhook will be sent to anywhere you want!"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Blane Townsend"
+plugin_author = "Blane Townsend (with thywolf's fix)"
 
 # The plugin's author's mail address.
 plugin_author_email = "blane.townsend@gmail.com"
